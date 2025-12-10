502563de8a14c2885f184a538663e0a8f3ecb8f7
ChenDa <cd050609_2023@qq.com>
2025-12-11 00:44:07 +0800

    test

---
+++ src/cd/DeviceGroup.java
@@ -0,0 +1,54 @@
+package cd;
+import equipment.HomeAppliances;
+import equipment.Equipment;
+import java.util.ArrayList;
+import java.util.List;
+
+public  class DeviceGroup extends Equipment implements HomeAppliances {
+//    private String groupName;
+    private List<Equipment> equipments;
+    public DeviceGroup(String groupName) {
+        super("设备组", groupName);
+        this.equipments = new ArrayList<>();
+    }
+    // 添加设备/设备组
+    public void addComponent(Equipment equipment ) {
+        equipments.add(equipment);
+        System.out.println("设备组[" + this.getName() + "]已添加组件："+equipment.getName() );
+    }
+
+    // 移除设备/设备组
+    public void removeComponent(Equipment equipment ) {
+        equipments.remove(equipment);
+        System.out.println("设备组[" + this.getName() + "]已移除组件："+ equipment.getName() );
+    }
+
+    public void selfCheck() {
+      //  System.out.println(groupName+"  selfCheck");
+    }
+
+    @Override
+    public void register() {
+       // System.out.println(groupName+"  register");
+    }
+
+    @Override
+    public void activate() {
+       // System.out.println(groupName+"  activate");
+    }
+    public void startSelf(){
+    for (Equipment equipment : equipments){
+        equipment.start();
+
+    }
+    }
+
+    /**
+     * 特定设备关闭
+     */
+    public void stopSelf() {
+    for (Equipment equipment : equipments){
+    equipment.stop();
+    }
+    }
+}
